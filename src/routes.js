7f07cc7de724eadabacf365c1c03299254a41aed
OlhaLu <olhalutsyshyna@gmail.com>
2020-06-10 13:51:01 +0300

    Rename routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,6 @@ export default {
   MOVIE_DETAILS_PAGE: '/movies/:movieId',
   MOVIE_CAST_PAGE: '/movies/:movieId/cast',
   MOVIE_REVIEWS_PAGE: '/movies/:movieId/reviews',
-  ACTORS_PAGE: '/person',
-  ACTOR_DETAILS_PAGE: '/person/:actorId',
+  ACTORS_PAGE: '/actors',
+  ACTOR_DETAILS_PAGE: '/actors/:actorId',
 };
